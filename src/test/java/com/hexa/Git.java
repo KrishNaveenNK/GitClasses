9facb25712f043cbe6df7845eb41fd875d866f21
KrishNaveenNK <krishnaveen021@gmail.com>
2022-11-07 18:45:42 +0530

    methods added to branch

--- src/test/java/com/hexa/Git.java
+++ src/test/java/com/hexa/Git.java
@@ -1,12 +1,17 @@
-/**
- * 
- */
+
 package com.hexa;
 
-/**
- * @author Krish Naveen
- *
- */
+
 public class Git {
 
+	private void method1() {
+System.out.println("method1");
+	}
+	private void method2() {
+		System.out.println("method1");
+	}
+	private void method3() {
+		System.out.println("method1");
+	}
+	
 }
