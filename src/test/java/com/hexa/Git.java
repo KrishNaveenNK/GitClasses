b63f2abd38e810025d0c4f060d7c2b6082eef32e
KrishNaveenNK <krishnaveen021@gmail.com>
2022-11-07 18:47:59 +0530

    methods added

--- src/test/java/com/hexa/Git.java
+++ src/test/java/com/hexa/Git.java
@@ -4,13 +4,13 @@ package com.hexa;
 
 public class Git {
 
-	private void method1() {
+	public void method1() {
 System.out.println("method1");
 	}
-	private void method2() {
+	public void method2() {
 		System.out.println("method1");
 	}
-	private void method3() {
+	public void method3() {
 		System.out.println("method1");
 	}
 	
